97580d23a7d1babf9a11efc2e5c032fd614df353
KARETI VENKATA VINAY KUMAR <105053576+VinaykumarKareti@users.noreply.github.com>
2024-05-28 14:44:41 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
 echo "Hi"
-# containerId=$(docker ps | awk 'NR==2 {print $1}')
-# echo "--------------------------------------------Container ID: $containerId"
-# docker rm $containerId
-
-
-
-# #!/bin/bash
-# echo "Hi"
-# containerId= $(docker ps | awk 'NR==2 {print $1}')
-# echo "--------------------->$containerId<----------------------------------------"
-# docker rm -f $containerId
-
